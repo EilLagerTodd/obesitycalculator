3b4d5deb42ac4d8f00751a299d243ed6dfde8050
hariose <67162120+hariose@users.noreply.github.com>
2021-09-17 00:40:17 +0900

    9.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,84 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Retrieve Text input',
-      home: MyCustomForm(),
+      title: 'FlutterDemo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: BmiMain(),
     );
   }
 }
 
-class MyCustomForm extends StatefulWidget {
+class BmiMain extends StatefulWidget {
+  const BmiMain({Key? key}) : super(key: key);
+
   @override
-  _MyCustomFormState createState() => _MyCustomFormState();
+  _BmiMainState createState() => _BmiMainState();
 }
 
-class _MyCustomFormState extends State<MyCustomForm> {
-  final myController = TextEditingController();
+class _BmiMainState extends State<BmiMain> {
 
-  @override
-  void initState() {
-    super.initState();
-
-    myController.addListener(_printLatesValue);
-  }
-
-  @override
-  void dispose() {
-    myController.dispose();
-    super.dispose();
-  }
-
-  _printLatesValue() {
-    print("두번째 text field: ${myController.text}");
-  }
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("text input 연습")),
-      body: Padding(
+      appBar: AppBar(title: Text("비만도 계산기")),
+      body: Container(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onChanged: (text) {
-                print("첫 번째 text field: $text");
-              },
-            ),
-            TextField(
-              controller: myController,
-            )
-          ],
+        child: Form(
+          key:_formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "키"
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "몸무게",
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: (){
+                    if (_formKey.currentState!.validate()){
+
+                    }
+                  },
+                  child: Text("결과"),
+                ),
+              )
+            ],
+          )
         ),
-      ),
+      )
     );
   }
 }
+
+class BmiResult extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
